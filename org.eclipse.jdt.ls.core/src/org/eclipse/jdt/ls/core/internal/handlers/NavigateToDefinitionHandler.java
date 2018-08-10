dcd2a126632106d888132cb3c65897c63158bd1a
Peterson Xu <932446461@qq.com>
2018-08-10 11:50:25 +0800

    [Java Lsp] construct symbol location for cross-repo goto definition (#3)

--- org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/handlers/NavigateToDefinitionHandler.java
+++ org.eclipse.jdt.ls.core/src/org/eclipse/jdt/ls/core/internal/handlers/NavigateToDefinitionHandler.java
@@ -20,6 +20,13 @@ import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.ITypeRoot;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.IBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.NodeFinder;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.manipulation.CoreASTProvider;
 import org.eclipse.jdt.ls.core.internal.JDTUtils;
 import org.eclipse.jdt.ls.core.internal.JavaLanguageServerPlugin;
 import org.eclipse.jdt.ls.core.internal.preferences.PreferenceManager;
@@ -40,6 +47,29 @@ public class NavigateToDefinitionHandler {
 		if (unit != null && !monitor.isCanceled()) {
 			location = computeDefinitionNavigation(unit, position.getPosition().getLine(),
 					position.getPosition().getCharacter(), monitor);
+		}
+		// TODO(pcxu) decouple these code
+		// if location is null use symbol://qualifedName else use git://...
+		try {
+			if (location == null) {
+				CompilationUnit ast = CoreASTProvider.getInstance().getAST(unit, CoreASTProvider.WAIT_YES, monitor);
+				int offset = JsonRpcHelpers.toOffset(unit.getBuffer(), position.getPosition().getLine(), position.getPosition().getCharacter());
+				if (ast == null || offset < 0) {
+					return null;
+				}
+				NodeFinder finder = new NodeFinder(ast, offset, 0);
+				ASTNode coveringNode = finder.getCoveringNode();
+				if (coveringNode instanceof SimpleName) {
+					IBinding resolvedBinding = ((SimpleName) coveringNode).resolveBinding();
+					if (resolvedBinding instanceof ITypeBinding) {
+						String qualifedName = ((ITypeBinding) resolvedBinding).getQualifiedName();
+						String uri = String.format("symbol://%s", qualifedName);
+						location = new Location(uri, JDTUtils.newRange());
+					}
+				}
+			}
+		} catch (JavaModelException e) {
+			JavaLanguageServerPlugin.logException("Problem computing definition for" + unit.getElementName(), e);
 		}
 		return location == null ? null : Arrays.asList(location);
 	}
