5baa0e994222eed11d893b730ac9ef78d51662c3
Pengcheng Xu <932446461@qq.com>
2018-09-26 14:18:08 +0800

    add edefinition method (#12)

---
+++ org.elastic.jdt.ls.core/src/org/elastic/jdt/ls/core/internal/EDefinitionHandler.java
@@ -0,0 +1,48 @@
+package org.elastic.jdt.ls.core.internal;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.ITypeRoot;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.ls.core.internal.JDTUtils;
+import org.eclipse.jdt.ls.core.internal.JavaLanguageServerPlugin;
+import org.eclipse.jdt.ls.core.internal.handlers.DocumentSymbolHandler;
+import org.eclipse.jdt.ls.core.internal.handlers.NavigateToDefinitionHandler;
+import org.eclipse.jdt.ls.core.internal.hover.JavaElementLabelComposer;
+import org.eclipse.jdt.ls.core.internal.hover.JavaElementLabels;
+import org.eclipse.jdt.ls.core.internal.preferences.PreferenceManager;
+import org.eclipse.lsp4j.Location;
+import org.eclipse.lsp4j.TextDocumentPositionParams;
+
+@SuppressWarnings("restriction")
+public class EDefinitionHandler extends NavigateToDefinitionHandler {
+
+	private PreferenceManager preferenceManager;
+
+	public EDefinitionHandler(PreferenceManager preferenceManager) {
+		super(preferenceManager);
+		this.preferenceManager = preferenceManager;
+	}
+
+	public SymbolLocator eDefinition(TextDocumentPositionParams position, IProgressMonitor monitor) {
+		List<? extends Location> locations = this.definition(position, monitor);
+		if (locations != null) {
+			return new SymbolLocator(locations.get(0));
+		}
+		ITypeRoot unit = JDTUtils.resolveTypeRoot(position.getTextDocument().getUri());
+		try {
+			IJavaElement element = JDTUtils.findElementAtSelection(unit, position.getPosition().getLine(), position.getPosition().getCharacter(), this.preferenceManager, monitor);
+			if (element == null) {
+				return null;
+			}
+			return new SymbolLocator(JavaElementLabels.getTextLabel(element, JavaElementLabels.ALL_FULLY_QUALIFIED), DocumentSymbolHandler.mapKind(element));
+		} catch (JavaModelException e) {
+			JavaLanguageServerPlugin.logException("Problem computing edefinition for" +  unit.getElementName(), e);
+		}
+		return null;
+	}
+
+}
