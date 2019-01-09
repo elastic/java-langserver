0e72ea80f244009ae0ac159f9e03b316ece4e53f
Poytr1 <pengcheng.xu@elastic.co>
2019-01-09 20:17:19 +0800

    use maven docker image

--- ci/script/docker/cleanup.sh
+++ ci/script/docker/cleanup.sh
@@ -4,6 +4,6 @@ docker stop $(docker ps -a -q)
 docker rm -v $(docker ps -a -q)
 docker volume prune -f
 
-[-e ./repository] && rm -rf ./repository
+[ -e ./repository ] && rm -rf ./repository
 
 exit 0
--- ci/script/docker/package_snapshot.sh
+++ ci/script/docker/package_snapshot.sh
@@ -8,22 +8,16 @@ elif [[ -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
     echo "AWS_SECRET_ACCESS_KEY is undefined"
     exit 1
 fi
-# run maven test first
-./mvnw clean verify 
 
-# deploy jdt language server to local 
-./mvnw -DskipTests=true clean deploy -DaltDeploymentRepository=dev::default::file:./repository -B -e -Pserver-distro 
+docker build --rm -f "ci/Dockerfile" -t code-lsp-java-langserver-ci:latest ci
 
-alias aws='docker run --rm -t $(tty &>/dev/null && echo "-i") -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" -e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" -e -v "$(pwd):/project" mesosphere/aws-cli'
-
-RELEASE=$(find ./repository/org/elastic/jdt/ls/org.elastic.jdt.ls.product/ -type f -name "org.elastic.jdt.ls.product*.tar.gz")
-if [ ${#RELEASE[@]} -eq 0 ]; then
-    echo "cannot find any packages"
-    exit 1
-elif [ ${#RELEASE[@]} -gt 1 ]; then 
-    echo "the actual number of packages is more than one"
-    exit 1
-else 
-    echo "upload ${RELEASE[0]} to s3..."
-    aws s3 cp ${RELEASE[0]} s3://download.elasticsearch.org/code/java-langserver/snapshot/
-fi
+docker run \
+    --rm -t $(tty &>/dev/null && echo "-i") \
+    -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
+    -e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
+    -v "$(pwd):/project" \
+    -v "$HOME/.m2":/root/.m2 \
+    code-lsp-java-langserver-ci \
+    /bin/bash -c "mvn clean verify -B -e && \
+                  mvn -DskipTests=true clean deploy -DaltDeploymentRepository=dev::default::file:./repository -B -e -Pserver-distro && \
+                  find ./repository/org/elastic/jdt/ls/org.elastic.jdt.ls.product/ -type f -name \"org.elastic.jdt.ls.product*.tar.gz\" -print0 | xargs -0 -I{} aws s3 cp {} s3://download.elasticsearch.org/code/java-langserver/snapshot/"
