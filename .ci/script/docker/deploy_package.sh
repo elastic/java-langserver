9737136ac08104b977fd682bf066a0f9a4f8eb6d
Poytr1 <pengcheng.xu@elastic.co>
2019-03-01 17:32:21 +0800

    tmp

--- .ci/script/docker/deploy_package.sh
+++ .ci/script/docker/deploy_package.sh
@@ -30,9 +30,9 @@ fi
 
 # alias aws='docker run --rm -t $(tty &>/dev/null && echo "-i") -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" -e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" -e "AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}" -v "$(pwd):/project" mesosphere/aws-cli'
 
-curl -o /usr/local/bin/aws https://raw.githubusercontent.com/mesosphere/aws-cli/master/aws.sh && chmod a+x /usr/local/bin/aws
-
 touch test.sh
+
+pip install awscli
 
 aws s3 cp test.sh s3://download.elasticsearch.org/code/java-langserver/$DESTINATION
                   
