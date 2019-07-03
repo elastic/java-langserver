a2df801cccf1dc58298a64e1a096d70558a51e08
Poytr1 <pengcheng.xu@elastic.co>
2019-07-03 21:21:52 +0800

    Bump version: 7.3.0-SNAPSHOT → 7.4.0-SNAPSHOT

--- .ci/script/docker/package.sh
+++ .ci/script/docker/package.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ $# -eq 0 ]; then
     echo "deploy snapshot package.."
-    KIBANA_VERSION=7.3.0
+    KIBANA_VERSION=7.4.0
     DESTINATION=snapshot/
     CMD="./mvnw clean verify -B -e"
 elif [ $# -eq 2 ]; then
