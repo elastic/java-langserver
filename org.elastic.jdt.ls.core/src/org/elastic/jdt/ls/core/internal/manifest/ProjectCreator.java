b1a8e1909645dd7dee8972d197dfd0db91d192b1
Poytr1 <pengcheng.xu@elastic.co>
2019-07-24 11:42:26 +0800

    Fix multi thread issue and create project

--- org.elastic.jdt.ls.core/src/org/elastic/jdt/ls/core/internal/manifest/ProjectCreator.java
+++ org.elastic.jdt.ls.core/src/org/elastic/jdt/ls/core/internal/manifest/ProjectCreator.java
@@ -38,7 +38,7 @@ import org.elastic.jdt.ls.core.internal.manifest.model.Dependency;
 import org.elastic.jdt.ls.core.internal.manifest.model.ProjectInfo;
 import org.elastic.jdt.ls.core.internal.manifest.model.Repo;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
@@ -83,7 +83,7 @@ public class ProjectCreator {
 			}
 			
 			if (project.getDependencies() != null) {
-				List<Pair<String, String>> dependencies = retrieveAllDeps(project.getDependencies(), project.getRepos());
+				List<ImmutablePair<String, String>> dependencies = retrieveAllDeps(project.getDependencies(), project.getRepos());
 				setClasspath(dependencies, javaProject, monitor);
 			}
 
@@ -92,26 +92,31 @@ public class ProjectCreator {
 			return javaProject;
 		}
 		catch (CoreException | InterruptedException ce) {
-			ElasticJavaLanguageServerPlugin.logException("Failed to create the java project depending on info" + project.toString(), ce);
+			ElasticJavaLanguageServerPlugin.logException("Failed to create the java project depending on info " + project.toString(), ce);
 			return null;
 		}
 	}
 
-	private void setClasspath(List<Pair<String, String>> dependencies, IJavaProject javaProject, IProgressMonitor monitor) throws CoreException {
+	private void setClasspath(List<ImmutablePair<String, String>> dependencies, IJavaProject javaProject, IProgressMonitor monitor) throws CoreException {
 		if (dependencies.size() == 0) {
 			return;
 		}
+		ElasticJavaLanguageServerPlugin.logInfo(dependencies.toString());
 		for (int i = 0; i < dependencies.size(); i++) {
-			String cp = dependencies.get(i).getLeft();
-			File classpathEntry = new File(cp);
-			if (classpathEntry.isFile()) {
-				if (dependencies.get(i).getRight() != null) {
-					addLibrary(javaProject, new Path(classpathEntry.getAbsolutePath()), new Path(new File(dependencies.get(i).getRight()).getAbsolutePath()), monitor);
+			try {
+				String cp = dependencies.get(i).getLeft();
+				File classpathEntry = new File(cp);
+				if (classpathEntry.isFile()) {
+					if (dependencies.get(i).getRight() != null) {
+						addLibrary(javaProject, new Path(classpathEntry.getAbsolutePath()), new Path(new File(dependencies.get(i).getRight()).getAbsolutePath()), monitor);
+					} else {
+						addLibrary(javaProject, new Path(classpathEntry.getAbsolutePath()), null, monitor);
+					}
 				} else {
-					addLibrary(javaProject, new Path(classpathEntry.getAbsolutePath()), null, monitor);
+					addContainer(javaProject, new Path(classpathEntry.getAbsolutePath()), monitor);
 				}
-			} else {
-				addContainer(javaProject, new Path(classpathEntry.getAbsolutePath()), monitor);
+			} catch (JavaModelException e) {
+				ElasticJavaLanguageServerPlugin.logException("Failed to add lib " + dependencies.get(i).toString(), e);
 			}
 		}
 	}
@@ -129,29 +134,32 @@ public class ProjectCreator {
 		}
 	}
 	
-	private List<Pair<String, String>> retrieveAllDeps(List<Dependency> deps, List<Repo> repos) throws InterruptedException {
-		List<Pair<String, String>> allDepsPaths = Collections.synchronizedList(new ArrayList<>());
-		
-		RepositorySystem system = ArtifactResolver.newRepositorySystem();
-        RepositorySystemSession session = ArtifactResolver.newRepositorySystemSession(system);
-
-        ArtifactRequest artifactRequest = new ArtifactRequest();
-        artifactRequest.setRepositories(ArtifactResolver.newRepositories(system, session, repos));
+	private List<ImmutablePair<String, String>> retrieveAllDeps(List<Dependency> deps, List<Repo> repos) throws InterruptedException {
+		List<ImmutablePair<String, String>> allDepsPaths = Collections.synchronizedList(new ArrayList<>());
         
         class DownloadDepTask implements Runnable {
-    		Dependency dep;
+			private Dependency dep;
+			private RepositorySystem system;
+			private RepositorySystemSession session;
+			private ArtifactRequest artifactRequest;
+
     		DownloadDepTask(Dependency dep) {
-    			this.dep = dep;
+				this.dep = dep;
+				this.system = ArtifactResolver.newRepositorySystem();
+				this.session = ArtifactResolver.newRepositorySystemSession(system);
+
+				this.artifactRequest = new ArtifactRequest();
+				this.artifactRequest.setRepositories(ArtifactResolver.newRepositories(system, session, repos));
 			}
     		@Override
     		public void run() {
     			File artifactFile = null;
             	if (dep.getPath() != null) {
             		// local libs
-            		allDepsPaths.add(Pair.of(dep.getPath(), null));
+            		allDepsPaths.add(ImmutablePair.of(dep.getPath(), null));
             	} else {
             		try {
-    	        		Artifact artifact = new DefaultArtifact(String.format("%s:%s:%s", dep.getGroupId(), dep.getArtifactId(), dep.getVersion()));
+						Artifact artifact = new DefaultArtifact(String.format("%s:%s:%s", dep.getGroupId(), dep.getArtifactId(), dep.getVersion()));
     	    			artifactRequest.setArtifact(artifact);
     	    			ArtifactResult artifactResult = system.resolveArtifact(session, artifactRequest);
     	    			artifactFile = artifactResult.getArtifact().getFile();
@@ -162,7 +170,7 @@ public class ProjectCreator {
     	            		Artifact sourceArtifact = new DefaultArtifact(String.format("%s:%s:jar:sources:%s", dep.getGroupId(), dep.getArtifactId(), dep.getVersion()));
     	            		artifactRequest.setArtifact(sourceArtifact);
     	            		ArtifactResult sourceArtifactResult = system.resolveArtifact(session, artifactRequest);
-    	            		allDepsPaths.add(Pair.of(artifactFile.getPath(), sourceArtifactResult.getArtifact().getFile().getPath()));
+    	            		allDepsPaths.add(ImmutablePair.of(artifactFile.getPath(), sourceArtifactResult.getArtifact().getFile().getPath()));
     	            	}
             		} catch (ArtifactResolutionException e) {
             			if (artifactFile != null) {
@@ -170,7 +178,7 @@ public class ProjectCreator {
         	            		// extract all *.aar to jar
         	            		explodeAarJarFiles(artifactFile, String.format("%s-%s-%s", dep.getGroupId(), dep.getArtifactId(), dep.getVersion())).forEach(allDepsPaths::add);
         	            	} else {
-        	            		allDepsPaths.add(Pair.of(artifactFile.getPath(), null));
+        	            		allDepsPaths.add(ImmutablePair.of(artifactFile.getPath(), null));
         	            	}
             			}
             		}
@@ -191,7 +199,7 @@ public class ProjectCreator {
 	
 	
 	// @see com.greensopinion.gradle.android.eclipse.GenerateLibraryDependenciesAction#explodeAarJarFiles
-	private Stream<Pair<String, String>> explodeAarJarFiles(File aarFile, String jarId) {
+	private Stream<ImmutablePair<String, String>> explodeAarJarFiles(File aarFile, String jarId) {
 		File targetFolder = new File(new File(new File(currentDir.toAbsolutePath().toString(), "build"), "exploded-aars"), jarId);
 		if (!targetFolder.exists()) {
 			if (!targetFolder.mkdirs()) {
@@ -215,7 +223,7 @@ public class ProjectCreator {
 			}
 		}
 		List<File> files = listFilesTraversingFolders(targetFolder);
-		return files.stream().filter(f -> f.getName().endsWith(".jar")).map(f -> Pair.of(f.getPath(), null)); 
+		return files.stream().filter(f -> f.getName().endsWith(".jar")).map(f -> ImmutablePair.of(f.getPath(), null)); 
 	}
 	
 	private List<File> listFilesTraversingFolders(File folder) {
@@ -261,15 +269,11 @@ public class ProjectCreator {
 
 	private IJavaProject createJavaProject(String projectName, IProgressMonitor monitor) throws CoreException {
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 
 		IProject project = root.getProject(projectName);
-		IProjectDescription description = workspace.newProjectDescription(project.getName());
-		
-		description.setLocationURI(currentDir.toUri());
-		
+
 		if (!project.exists()) {
-			project.create(description, monitor);
+			project.create(monitor);
 		} else {
 			project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
 		}
@@ -283,6 +287,8 @@ public class ProjectCreator {
 		}
 
 		IJavaProject jproject = JavaCore.create(project);
+
+		jproject.setRawClasspath(new IClasspathEntry[0], monitor);
 
 		return jproject;
 	}
