ba92f57c8d8dbfc0dabbcd02f2750f3c75be8876
Mengwei Ding <mengwei.ding@gmail.com>
2019-08-22 09:30:42 -0700

    bump to 7.3.2 (#95)

--- .ci/script/docker/package.sh
+++ .ci/script/docker/package.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ $# -eq 0 ]; then
     echo "deploy snapshot package.."
-    KIBANA_VERSION=7.3.1
+    KIBANA_VERSION=7.3.2
     DESTINATION=snapshot/
     CMD="./mvnw clean verify -B -e"
 elif [ $# -eq 2 ]; then
